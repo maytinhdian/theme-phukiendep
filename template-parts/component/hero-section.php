936a47764b5b3e94fb27e9992578874a33ef190b
Minh Triet <minhtriet@maytinhdian.com>
2025-05-04 14:06:58 +0700

    Update header-top  - hero-section

--- template-parts/component/hero-section.php
+++ template-parts/component/hero-section.php
@@ -1,5 +1,12 @@
 <div class="hero-section">
     <div class="hero-section__content">
-       <h1>Test template</h1>
+        <div class="content content__info">
+            <h1><span>cần</span> <span class="subheading">Laptop</span></h1>
+            <h1>shop <span class="subheading">CÓ</span></h1>
+            <h2>mới 99%</h2>
+        </div>
+        <div class="content content__img">
+            <img src="http://phukiendep.local/wp-content/uploads/2025/05/Laptop400x400.png" alt="Hero Image" class="hero-image" />
+        </div>
     </div>
 </div>
--- template-parts/header/header-top.php
+++ template-parts/header/header-top.php
@@ -3,6 +3,10 @@
         <a href="#"><i class="fa-solid fa-phone fa-shake"></i><?php echo wp_kses_post(get_theme_mod('cellphone_number_setting', '')) ?></a>
     </div>
     <div class="header-top__logon">
+    <!-- <a href="#"></i><?php echo wp_kses_post(get_theme_mod('cellphone_number_setting', '')) ?><i class="fa-solid fa-right-to-bracket fa-xl"></i></a> -->
+    <a href="<?php echo esc_url( wp_login_url() ); ?>" alt="<?php esc_attr_e( 'Login', 'phukiendep' ); ?>">
+        <?php _e( 'Login', 'phukiendep' );?>
         <i class="fa-solid fa-right-to-bracket fa-xl"></i>
+</a>
     </div>
 </div>
