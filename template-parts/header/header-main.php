c96fb6cc8b2ff1381ffff8ead41d0ddbf84570a6
Minh Triet <minhtriet@maytinhdian.com>
2025-05-05 06:37:43 +0700

    Header continue

--- template-parts/header/header-main.php
+++ template-parts/header/header-main.php
@@ -1,10 +1,21 @@
 <div class="header-main">
-    <?php 
-    // Check if the current page is the home page
-    if(is_front_page() || is_home()) {
-        //Display the logo
-        the_custom_logo();
-    }
-    
-    ?>
+    <div class="header-main__site-branding">
+        <?php
+        // Check if the current page is the home page
+        if (is_front_page() || is_home()) {
+            //Display the logo
+            the_custom_logo();
+        }
+
+        ?>
+
+    </div>
+    <div class="header-main__searchbar">
+        <form role="search" method="get" class="search-form" action="<?php echo esc_url(home_url('/')); ?>">
+            <button type="submit" class="search-submit"><i class="fa-solid fa-magnifying-glass"></i></button>
+            <label>
+                <span class="screen-reader-text"><?php echo _x('Search for:', 'label'); ?></span>
+                <input type="search" id="search" class="search-field" placeholder="<?php echo esc_attr_x('Search &hellip;', 'placeholder'); ?>" value="<?php echo get_search_query(); ?>" name="s" title="<?php echo esc_attr_x('Search for:', 'label'); ?>" />
+            </label>
+    </div>
 </div>
