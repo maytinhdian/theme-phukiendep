d0c3593514ab50b9a93293e0afbbd1de102b7946
maytinhdian <tnhalk@maytinhdian.com>
2025-05-08 19:36:48 +0700

    Account Profile Css

---
+++ template-parts/component/account-none.php
@@ -0,0 +1,26 @@
+<?php if (is_user_logged_in()) {
+    $current_user = wp_get_current_user();
+?>
+    <div class="account">
+        <div class="account-avatar">
+            <img src="<?php get_avatar_url(get_current_user_id()) ;?>" alt="<?php echo $current_user->display_name; ?>">
+        </div>
+        <div class="account-exit">
+            <a href="<?php echo esc_url(wp_logout_url(get_permalink())); ?>"><i class="fa-solid fa-right-from-bracket"></i></a>
+        </div>
+    </div>
+
+<?php
+} else {
+?>
+    <div class="account">
+        <div class="account-avatar">
+            Hello Guest
+        </div>
+        <div class="account-exit">
+            <a href="<?php echo esc_url(wp_login_url()); ?>"><i class="fa-solid fa-right-to-bracket fa-xl"></i></a>
+        </div>
+    </div>
+
+<?php
+};
--- template-parts/header/header-top.php
+++ template-parts/header/header-top.php
@@ -3,26 +3,15 @@
         <a href="#"><i class="fa-solid fa-phone fa-shake"></i><?php echo wp_kses_post(get_theme_mod('cellphone_number_setting', '')) ?></a>
     </div>
     <div class="header-top__logon">
-        <!-- <a href="#"></i><?php echo wp_kses_post(get_theme_mod('cellphone_number_setting', '')) ?><i class="fa-solid fa-right-to-bracket fa-xl"></i></a> -->
+        <!-- <?php if (is_user_logged_in()) {
+                    $current_user = wp_get_current_user();
+                    echo $current_user->display_name . ' <a href="' . esc_url(wp_logout_url(get_permalink())) . '"><i class="fa-solid fa-right-from-bracket"></i></a>';
+                } else {
+                    echo ' <a href="' . esc_url(wp_login_url()) . '" >Guest <i class="fa-solid fa-right-to-bracket fa-xl"></i></a>';
+                }; ?> -->
 
-
-
-
-        <!-- <a href="<?php echo esc_url(wp_login_url()); ?>" alt="<?php esc_attr_e('Login', 'phukiendep'); ?>"> -->
-            <?php if (is_user_logged_in()) {
-                 $current_user = wp_get_current_user();
-                 echo $current_user->display_name . ' <a href="' . esc_url(wp_logout_url(get_permalink())) . '"><i class="fa-solid fa-right-from-bracket"></i></a>';
-            } else {
-                echo ' <a href="' . esc_url(wp_login_url()) . '" >Guest <i class="fa-solid fa-right-to-bracket fa-xl"></i></a>';
-            }; ?>
-
-        <!-- </a> -->
+        <?php
+        get_template_part('template-parts/component/account', 'none', array());
+        ?>
     </div>
 </div>
-<?php
-// if (is_user_logged_in()) {
-//     $current_user = wp_get_current_user();
-//     echo $current_user->display_name . ' <a href="' . esc_url(wp_logout_url()) . '">Logout</a>';
-// } else {
-//     echo 'Welcome, visitor!';
-// }; ?>
