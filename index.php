4b41ad6631791a907c275d2c99eb5e6e225cee19
maytinhdian <tnhalk@maytinhdian.com>
2025-05-09 13:29:47 +0700

    blog css update

--- index.php
+++ index.php
@@ -17,49 +17,19 @@ get_header();
 ?>
 
 <main id="primary" class="site-main">
-	<?php
-	/* Start the Loop */
-	while (have_posts()) :
-		the_post();
-	?>
-		<div class="post">
-<<<<<<< HEAD
-			<div class=" post-title">
-=======
-			<div class="post post-title">
->>>>>>> 65a2eb3f48dcd34de54d17b41793c1e5eefa5506
-				<h2 class="post-title__heading"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
-				<div class="post-title__meta">
-					<span class="post-title__meta--author">By <?php the_author(); ?></span>
-					<span class="post-title__meta--date"><?php echo get_the_date(); ?></span>
-					<span class="post-title__meta--category"><?php the_category(', '); ?></span>
-				</div>
-				<div class="post-content">
-					<div class="post-content__img">
-						<?php if (has_post_thumbnail()) : ?>
-							<a href="<?php the_permalink(); ?>"><?php the_post_thumbnail('medium'); ?></a>
-						<?php endif; ?>
-					</div>
-					<div class="post-content__text">
-						<?php the_excerpt(); ?>
-					</div>
-					<p class="post-content__read-more">
-<<<<<<< HEAD
-						<a href="<?php the_permalink(); ?>" class="">Read More >>></a>
-=======
-						<a href="<?php the_permalink(); ?>" class="">Read More</a>
->>>>>>> 65a2eb3f48dcd34de54d17b41793c1e5eefa5506
-					</p>
-				</div>
-			</div>
+	<div class="post-section">
 
 		<?php
+		/* Start the Loop */
+		while (have_posts()) :
+			the_post();
+			get_template_part('template-parts/component/post', 'none', array());
+		endwhile;
 
-	endwhile;
-
-	the_posts_navigation();
+		the_posts_navigation();
 
 		?>
+	</div>
 
 </main><!-- #main -->
 
---
+++ template-parts/component/post-none.php
@@ -0,0 +1,24 @@
+<div class="post">
+    <div class="post-title">
+        <h2 class="post-title__heading"><a href="<?php the_permalink(); ?>"><?php $postId;                                                                          the_title(); ?></a></h2>
+        <div class="post-title__meta">
+            <span class="post-title__meta--author">By <?php the_author(); ?></span>
+            <span class="post-title__meta--date"><?php echo get_the_date(); ?></span>
+            <span class="post-title__meta--category"><?php the_category(', '); ?></span>
+        </div>
+    </div>
+    <div class="post-content">
+        <div class="post-content__img">
+            <?php if (has_post_thumbnail()) : ?>
+                <a href="<?php the_permalink(); ?>"><?php the_post_thumbnail('medium'); ?></a>
+            <?php endif; ?>
+        </div>
+        <div class="post-content__text">
+            <?php the_excerpt(); ?>
+        </div>
+        <div class="post-content__read-more">
+            <a href="<?php the_permalink(); ?>" class="">Read More>>></a>
+
+        </div>
+    </div>
+</div>
