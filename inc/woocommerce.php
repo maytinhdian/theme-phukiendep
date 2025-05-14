d9fd0acd209ea2132bd00a030bc35353421fb28f
Minh Triet <minhtriet@maytinhdian.com>
2025-05-14 07:17:05 +0700

    New woocommerce use hook

--- inc/woocommerce.php
+++ inc/woocommerce.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * WooCommerce Compatibility File
  *
@@ -16,11 +17,13 @@
  *
  * @return void
  */
-function phukiendep_woocommerce_setup() {
+add_action('after_setup_theme', 'phukiendep_woocommerce_setup');
+function phukiendep_woocommerce_setup()
+{
 	add_theme_support(
 		'woocommerce',
 		array(
-			'thumbnail_image_width' => 150,
+			'thumbnail_image_width' => 250,
 			'single_image_width'    => 300,
 			'product_grid'          => array(
 				'default_rows'    => 3,
@@ -31,19 +34,20 @@ function phukiendep_woocommerce_setup() {
 			),
 		)
 	);
-	add_theme_support( 'wc-product-gallery-zoom' );
-	add_theme_support( 'wc-product-gallery-lightbox' );
-	add_theme_support( 'wc-product-gallery-slider' );
+	add_theme_support('wc-product-gallery-zoom');
+	add_theme_support('wc-product-gallery-lightbox');
+	add_theme_support('wc-product-gallery-slider');
 }
-add_action( 'after_setup_theme', 'phukiendep_woocommerce_setup' );
 
 /**
  * WooCommerce specific scripts & stylesheets.
  *
  * @return void
  */
-function phukiendep_woocommerce_scripts() {
-	wp_enqueue_style( 'phukiendep-woocommerce-style', get_template_directory_uri() . '/woocommerce.css', array(), _S_VERSION );
+add_action('wp_enqueue_scripts', 'phukiendep_woocommerce_scripts');
+function phukiendep_woocommerce_scripts()
+{
+	wp_enqueue_style('phukiendep-woocommerce-style', get_template_directory_uri() . '/woocommerce.css', array(), _S_VERSION);
 
 	$font_path   = WC()->plugin_url() . '/assets/fonts/';
 	$inline_font = '@font-face {
@@ -57,9 +61,8 @@ function phukiendep_woocommerce_scripts() {
 			font-style: normal;
 		}';
 
-	wp_add_inline_style( 'phukiendep-woocommerce-style', $inline_font );
+	wp_add_inline_style('phukiendep-woocommerce-style', $inline_font);
 }
-add_action( 'wp_enqueue_scripts', 'phukiendep_woocommerce_scripts' );
 
 /**
  * Disable the default WooCommerce stylesheet.
@@ -69,7 +72,7 @@ add_action( 'wp_enqueue_scripts', 'phukiendep_woocommerce_scripts' );
  *
  * @link https://docs.woocommerce.com/document/disable-the-default-stylesheet/
  */
-add_filter( 'woocommerce_enqueue_styles', '__return_empty_array' );
+add_filter('woocommerce_enqueue_styles', '__return_empty_array');
 
 /**
  * Add 'woocommerce-active' class to the body tag.
@@ -77,12 +80,13 @@ add_filter( 'woocommerce_enqueue_styles', '__return_empty_array' );
  * @param  array $classes CSS classes applied to the body tag.
  * @return array $classes modified to include 'woocommerce-active' class.
  */
-function phukiendep_woocommerce_active_body_class( $classes ) {
+add_filter('body_class', 'phukiendep_woocommerce_active_body_class');
+function phukiendep_woocommerce_active_body_class($classes)
+{
 	$classes[] = 'woocommerce-active';
 
 	return $classes;
 }
-add_filter( 'body_class', 'phukiendep_woocommerce_active_body_class' );
 
 /**
  * Related Products Args.
@@ -90,138 +94,63 @@ add_filter( 'body_class', 'phukiendep_woocommerce_active_body_class' );
  * @param array $args related products args.
  * @return array $args related products args.
  */
-function phukiendep_woocommerce_related_products_args( $args ) {
+add_filter('woocommerce_output_related_products_args', 'phukiendep_woocommerce_related_products_args');
+function phukiendep_woocommerce_related_products_args($args)
+{
 	$defaults = array(
 		'posts_per_page' => 3,
 		'columns'        => 3,
 	);
 
-	$args = wp_parse_args( $defaults, $args );
+	$args = wp_parse_args($defaults, $args);
 
 	return $args;
 }
-add_filter( 'woocommerce_output_related_products_args', 'phukiendep_woocommerce_related_products_args' );
+
 
 /**
- * Remove default WooCommerce wrapper.
+ * Add main open tag to hook: woocommerce_before_main_content  
  */
-remove_action( 'woocommerce_before_main_content', 'woocommerce_output_content_wrapper', 10 );
-remove_action( 'woocommerce_after_main_content', 'woocommerce_output_content_wrapper_end', 10 );
+add_action('woocommerce_before_main_content', 'pkd_woocommerce_output_content_wrapper', 8);
+function pkd_woocommerce_output_content_wrapper()
+{
+	remove_action('woocommerce_before_main_content', 'woocommerce_output_content_wrapper');
+	
+	echo '<main id="product-main" class="product-main"> <!--Start #product-main-->';
+	echo '<div id="product-main__breadcrumb" class="product-main__breadcrumb"> <!--Start #product-main__breadcrumb-->';
+};
 
-if ( ! function_exists( 'phukiendep_woocommerce_wrapper_before' ) ) {
-	/**
-	 * Before Content.
-	 *
-	 * Wraps all WooCommerce content in wrappers which match the theme markup.
-	 *
-	 * @return void
-	 */
-	function phukiendep_woocommerce_wrapper_before() {
-		?>
-			<main id="primary" class="site-main">
-		<?php
-	}
-}
-add_action( 'woocommerce_before_main_content', 'phukiendep_woocommerce_wrapper_before' );
+add_action('woocommerce_before_main_content', 'pkd_woocommerce_remove_woocommerce_breadcrumb', 9);
+function pkd_woocommerce_remove_woocommerce_breadcrumb()
+{
+	remove_action('woocommerce_before_main_content', 'woocommerce_breadcrumb',20);
+	echo '<!--woocommerce_breadcrumb removed-->';
+};
 
-if ( ! function_exists( 'phukiendep_woocommerce_wrapper_after' ) ) {
-	/**
-	 * After Content.
-	 *
-	 * Closes the wrapping divs.
-	 *
-	 * @return void
-	 */
-	function phukiendep_woocommerce_wrapper_after() {
-		?>
-			</main><!-- #main -->
-		<?php
-	}
+
+add_action('woocommerce_shop_loop_header', 'pkd_woocommerce_add_breadcrumb', 10);
+function pkd_woocommerce_add_breadcrumb()
+{
+	echo '<!--woocommerce_breadcrumb insert-->';
+	add_action('woocommerce_shop_loop_header', 'woocommerce_breadcrumb',20);
+};
+/**
+ * Add div close tag to hook: woocommerce_breadcrumb  
+ */
+add_action('woocommerce_before_main_content', 'pkd_woocommerce_breadcrumb_wrapper_end', 22);
+function pkd_woocommerce_breadcrumb_wrapper_end()
+{
+	echo '</div"> <!--End #product-main__breadcrumb-->';
 }
-add_action( 'woocommerce_after_main_content', 'phukiendep_woocommerce_wrapper_after' );
+
+
 
 /**
- * Sample implementation of the WooCommerce Mini Cart.
- *
- * You can add the WooCommerce Mini Cart to header.php like so ...
- *
-	<?php
-		if ( function_exists( 'phukiendep_woocommerce_header_cart' ) ) {
-			phukiendep_woocommerce_header_cart();
-		}
-	?>
+ * Add main close tag to hook: woocommerce_after_main_content  
  */
-
-if ( ! function_exists( 'phukiendep_woocommerce_cart_link_fragment' ) ) {
-	/**
-	 * Cart Fragments.
-	 *
-	 * Ensure cart contents update when products are added to the cart via AJAX.
-	 *
-	 * @param array $fragments Fragments to refresh via AJAX.
-	 * @return array Fragments to refresh via AJAX.
-	 */
-	function phukiendep_woocommerce_cart_link_fragment( $fragments ) {
-		ob_start();
-		phukiendep_woocommerce_cart_link();
-		$fragments['a.cart-contents'] = ob_get_clean();
-
-		return $fragments;
-	}
-}
-add_filter( 'woocommerce_add_to_cart_fragments', 'phukiendep_woocommerce_cart_link_fragment' );
-
-if ( ! function_exists( 'phukiendep_woocommerce_cart_link' ) ) {
-	/**
-	 * Cart Link.
-	 *
-	 * Displayed a link to the cart including the number of items present and the cart total.
-	 *
-	 * @return void
-	 */
-	function phukiendep_woocommerce_cart_link() {
-		?>
-		<a class="cart-contents" href="<?php echo esc_url( wc_get_cart_url() ); ?>" title="<?php esc_attr_e( 'View your shopping cart', 'phukiendep' ); ?>">
-			<?php
-			$item_count_text = sprintf(
-				/* translators: number of items in the mini cart. */
-				_n( '%d item', '%d items', WC()->cart->get_cart_contents_count(), 'phukiendep' ),
-				WC()->cart->get_cart_contents_count()
-			);
-			?>
-			<span class="amount"><?php echo wp_kses_data( WC()->cart->get_cart_subtotal() ); ?></span> <span class="count"><?php echo esc_html( $item_count_text ); ?></span>
-		</a>
-		<?php
-	}
-}
-
-if ( ! function_exists( 'phukiendep_woocommerce_header_cart' ) ) {
-	/**
-	 * Display Header Cart.
-	 *
-	 * @return void
-	 */
-	function phukiendep_woocommerce_header_cart() {
-		if ( is_cart() ) {
-			$class = 'current-menu-item';
-		} else {
-			$class = '';
-		}
-		?>
-		<ul id="site-header-cart" class="site-header-cart">
-			<li class="<?php echo esc_attr( $class ); ?>">
-				<?php phukiendep_woocommerce_cart_link(); ?>
-			</li>
-			<li>
-				<?php
-				$instance = array(
-					'title' => '',
-				);
-
-				the_widget( 'WC_Widget_Cart', $instance );
-				?>
-			</li>
-		</ul>
-		<?php
-	}
-}
+add_action('woocommerce_after_main_content', 'pkd_woocommerce_output_content_wrapper_end', 8);
+function pkd_woocommerce_output_content_wrapper_end()
+{
+	remove_action('woocommerce_after_main_content', 'woocommerce_output_content_wrapper_end');
+	echo '</div> <!--End #product-main-->';
+};
