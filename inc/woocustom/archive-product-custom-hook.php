8340012ddfa582938b849bf18ffa81a9480b2cad
Minh Triet <minhtriet@maytinhdian.com>
2025-05-19 06:50:02 +0700

    Product Item Card

--- inc/woocustom/archive-product-custom-hook.php
+++ inc/woocustom/archive-product-custom-hook.php
@@ -1,4 +1,5 @@
-<?php 
+<?php
+
 /**
  * Hook: woocommerce_before_main_content.
  *
@@ -6,7 +7,68 @@
  * @hooked woocommerce_breadcrumb - 20
  * @hooked WC_Structured_Data::generate_website_data() - 30
  */
-// do_action( 'woocommerce_before_main_content' );
+remove_action('woocommerce_before_main_content', 'woocommerce_breadcrumb', 20);
+
+
+
+
+/**
+ * Hook: woocommerce_before_shop_loop.
+ * Thêm div bọc breadcrumb trong shop loop WooCommerce.
+ * Hook vào woocommerce_before_shop_loop - priority 8.
+ * 
+ * @hooked woocommerce_output_all_notices - 10
+ * @hooked woocommerce_result_count - 20
+ * @hooked woocommerce_catalog_ordering - 30
+ */
+add_action('woocommerce_before_shop_loop', 'pkd_woocommerce_breadcrumb', 8);
+function pkd_woocommerce_breadcrumb()
+{
+?>
+    <!-- Block: Breadcrumb Wrapper -->
+    <div class="product-breadcrumb"><!--* product-breadcrumb *-->
+        <?php
+        /**
+         * Hiển thị breadcrumb WooCommerce
+         */
+        woocommerce_breadcrumb();
+        ?>
+    </div><!--* End .product-breadcrumb *-->
+    <!-- End Block: Breadcrumb Wrapper -->
+<?php
+}
+add_action('woocommerce_before_shop_loop', 'pkd_woocommerce_product_loop_start_wrapper', 32);
+function pkd_woocommerce_product_loop_start_wrapper()
+{
+?>
+   
+    <div class="product-list"><!--Start #product-list -->
+ 
+<?php
+}
+
+
+
+/**
+ * Hook: woocommerce_after_shop_loop.
+ *
+ * @hooked woocommerce_pagination - 10
+ */
+add_action( 'woocommerce_after_shop_loop', 'pkd_woocommerce_product_loop_start_wrapper_end', 4 );
+function pkd_woocommerce_product_loop_start_wrapper_end(){
+   echo '  </div> <!--End #product-list --> ';
+}
+
+add_action('woocommerce_after_shop_loop', 'pkd_woocommerce_pagination_wrapper', 8);
+function pkd_woocommerce_pagination_wrapper()
+{
+    echo '<div class="product-pagination">';
+}
+add_action('woocommerce_after_shop_loop', 'pkd_woocommerce_pagination_wrapper_end', 12);
+function pkd_woocommerce_pagination_wrapper_end()
+{
+    echo '</div> <!---- End #product-pagination---->';
+}
 
 
 
--- inc/woocustom/content-product-custom-hook.php
+++ inc/woocustom/content-product-custom-hook.php
@@ -5,12 +5,19 @@
  *
  * @hooked woocommerce_template_loop_product_link_open - 10
  */
-add_action('woocommerce_before_shop_loop_item', 'pkd_woocommerce_template_loop_product_link_open', 8);
-function pkd_woocommerce_template_loop_product_link_open()
+add_action('woocommerce_before_shop_loop_item', 'pkd_woocommerce_template_loop_product_link_open_wrapper', 8);
+function pkd_woocommerce_template_loop_product_link_open_wrapper()
 {
 ?>
     <div class="product-card">
-        <div class="badge">Hot
+    <?php
+}
+add_action('woocommerce_before_shop_loop_item', 'pkd_woocommerce_template_loop_product_link_open_wrapper_end', 12);
+function pkd_woocommerce_template_loop_product_link_open_wrapper_end()
+{
+    ?>
+
+        <div class="badge">
             <?php
             woocommerce_show_product_loop_sale_flash();
             ?>
@@ -18,62 +25,63 @@ function pkd_woocommerce_template_loop_product_link_open()
         <div class="product-thumbnail">
             <div class="card-img">
                 <?php woocommerce_template_loop_product_thumbnail(); ?>
-            </div>
+            </div> <!--End #card-img-->
+
+        <?php
+    }
+
+    /**
+     * Hook: woocommerce_before_shop_loop_item_title.
+     *
+     * @hooked woocommerce_show_product_loop_sale_flash - 10
+     * @hooked woocommerce_template_loop_product_thumbnail - 10
+     */
+    remove_action('woocommerce_before_shop_loop_item_title', 'woocommerce_show_product_loop_sale_flash', 10);
+    remove_action('woocommerce_before_shop_loop_item_title', 'woocommerce_template_loop_product_thumbnail', 10);
+
+
+    /**
+     * Hook: woocommerce_after_shop_loop_item_title.
+     *
+     * @hooked woocommerce_template_loop_rating - 5
+     * @hooked woocommerce_template_loop_price - 10
+     */
+    add_action('woocommerce_after_shop_loop_item_title', 'pkd_woocommerce_after_shop_loop_item_title_wrapper', 9);
+    function pkd_woocommerce_after_shop_loop_item_title_wrapper()
+    {
+        echo ' <div class="product-price">';
+    }
+    add_action('woocommerce_after_shop_loop_item_title', 'pkd_woocommerce_after_shop_loop_item_title_wrapper_end', 11);
+    function pkd_woocommerce_after_shop_loop_item_title_wrapper_end()
+    {
+        echo '</div><!---End #product-price---->';
+    }
+
+    /**
+     * Hook: woocommerce_after_shop_loop_item.
+     *
+     * @hooked woocommerce_template_loop_product_link_close - 5
+     * @hooked woocommerce_template_loop_add_to_cart - 10
+     */
+    add_action('woocommerce_after_shop_loop_item', 'pkd_woocommerce_after_shop_loop_item', 3);
+    function pkd_woocommerce_after_shop_loop_item()
+    {
+        ?>
         </div> <!--End #product-thumbnail-->
     <?php
-}
-
-
-/**
- * Hook: woocommerce_before_shop_loop_item_title.
- *
- * @hooked woocommerce_show_product_loop_sale_flash - 10
- * @hooked woocommerce_template_loop_product_thumbnail - 10
- */
-remove_action('woocommerce_before_shop_loop_item_title', 'woocommerce_show_product_loop_sale_flash', 10);
-remove_action('woocommerce_before_shop_loop_item_title', 'woocommerce_template_loop_product_thumbnail', 10);
-
-
-
-
-
-/**
- * Hook: woocommerce_after_shop_loop_item.
- *
- * @hooked woocommerce_template_loop_product_link_close - 5
- * @hooked woocommerce_template_loop_add_to_cart - 10
- */
-add_action('woocommerce_after_shop_loop_item', 'pkd_woocommerce_after_shop_loop_item_wrapper_end', 12);
-function pkd_woocommerce_after_shop_loop_item_wrapper_end()
-{
+    }
+    add_action('woocommerce_after_shop_loop_item', 'pkd_woocommerce_template_loop_add_to_cart_wrapper', 9);
+    function pkd_woocommerce_template_loop_add_to_cart_wrapper()
+    {
     ?>
-
-        <div class="product-details">
-            <span class="product-catagory"><?php
-                                            global $product;
-                                            $terms = get_the_terms($product->get_id(), 'product_cat');
-                                            if ($terms && ! is_wp_error($terms)) {
-                                                $cat_names = array();
-                                                foreach ($terms as $term) {
-                                                    $cat_names[] = $term->name;
-                                                }
-                                                echo implode(', ', $cat_names);
-                                            }
-                                            ?></span>
-            <h4><a href="">Women leather bag</a></h4>
-            <p><? the_excerpt(); ?></p>
-            <div class="product-bottom-details">
-                <div class="product-price"><small><?php $regular_price = $product->get_regular_price();
-                                                    echo $regular_price; ?></small><?php global $product;
-                                                                                    $sale_price = $product->get_sale_price();
-                                                                                    echo $sale_price; ?></div>
-                <div class="product-links">
-                    <a href=""><i class="fa fa-heart"></i></a>
-                    <a href=""><i class="fa fa-shopping-cart"></i></a>
-                </div>
-            </div>
-        </div>
-    </div>
-    </div><!----End #product-card---->
-<?php
-}
+        <div class="product-cart>
+    <?php
+    }
+    add_action('woocommerce_after_shop_loop_item', 'pkd_woocommerce_template_loop_add_to_cart_wrapper_end', 11);
+    function pkd_woocommerce_template_loop_add_to_cart_wrapper_end()
+    {
+    ?>
+        </div> <!--End #product-cart-->
+         </div> <!--End #product-card-->
+    <?php
+    }
