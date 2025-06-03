6d348893e7e335b2358c9c2ebc5197e7fde325fd
Minh Triet <minhtriet@maytinhdian.com>
2025-06-03 08:03:30 +0700

    Home Update

--- template-parts/component/carousel-home.php
+++ template-parts/component/carousel-home.php
@@ -1,41 +1,11 @@
-<!DOCTYPE html>
-<html lang="en">
-
-<!-- Demo styles -->
-<style>
-  .swiper {
-    width: 98vw;
-    height: 100px;
-    border-radius: 0.5rem;
-    margin-bottom: 1rem;
-  }
-
-  .swiper-slide {
-    text-align: center;
-    font-size: 18px;
-    background: #444;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-  }
-
-  .swiper-slide img {
-    display: block;
-    width: 100%;
-    height: 100%;
-    object-fit: cover;
-  }
-</style>
-</head>
-
 
 <!-- Swiper -->
-<div class="swiper mySwiper">
+<div id="short-swiper" class="swiper mySwiper">
   <div class="swiper-wrapper">
-    <div class="swiper-slide">Slide 1</div>
-    <div class="swiper-slide">Slide 2</div>
-    <div class="swiper-slide">Slide 3</div>
-    <div class="swiper-slide">Slide 4</div>
+    <div class="swiper-slide"><img src="http://phukiendep.local/wp-content/uploads/2025/06/swiper1.png"></div>
+    <div class="swiper-slide"><img src="http://phukiendep.local/wp-content/uploads/2025/06/swiper2.png"></div>
+    <div class="swiper-slide"><img src="http://phukiendep.local/wp-content/uploads/2025/06/swiper3.png"></div>
+    <div class="swiper-slide"><img src="http://phukiendep.local/wp-content/uploads/2025/06/swiper4.png"></div>
     <div class="swiper-slide">Slide 5</div>
     <div class="swiper-slide">Slide 6</div>
     <div class="swiper-slide">Slide 7</div>
@@ -44,9 +14,3 @@
   </div>
   <div class="swiper-pagination"></div>
 </div>
-
-
-
-
-
-</html>
