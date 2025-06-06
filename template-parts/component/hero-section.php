fc0bfa337b0a45a5b73ed9341b2d15e5efa50815
Minh Triet <minhtriet@maytinhdian.com>
2025-06-06 17:01:35 +0700

    Home update P3

--- template-parts/component/hero-section.php
+++ template-parts/component/hero-section.php
@@ -1,22 +1,19 @@
-
 <div class="hero-section">
-    <div class="hero-section__title">
-        <h1 class="hero-section__heading">Laptop 99%</h1>
-        <p class="hero-section__description">Xem thêm >></p>
-        <div class="line-break"></div>
+    <div class="hero-section__header">
+        <div class="hero-section__categories">
+            <h1>laptop 99%</h1>
+        </div>
+        <div class="hero-section__see-more">
+            <a href="#">Xem thêm >></a>
+        </div>
+        <div class="hero-section__line-break"></div>
+    </div>
     <div class="hero-section__content">
         <div class="content__img">
-            <img src="http://phukiendep.local/wp-content/uploads/2025/05/Laptop600x400.png" alt="Hero Image" class="hero-image" />
+            <img src="<?php echo get_template_directory_uri(); ?>/assets/images/laptop600x400.png" alt="Hero Image" class="hero-image" />
         </div>
-        <div class="content__info">
-            <div class="slogan">
-                <span class="slogan">cần</span>
-                <span class="slogan subheading">Laptop</span>
-                <span class="slogan ">shop</span>
-                <span class="slogan subheading">CÓ</span>
-                <span class="slogan ">máy 99%</span>
-            </div>
-            <div class="see-more"><button class="slogan see-more__button" type="button">tìm hiểu thêm...</button></div>
+        <div class="content__right">
+            <img src="<?php echo get_template_directory_uri(); ?>/assets/images/laptop600x400.png" alt="Hero Image" class="hero-image" />
         </div>
     </div>
 </div>
