171393f29428ee0ec3120f89263d454c6424ad88
Minh Triet <minhtriet@maytinhdian.com>
2025-06-21 06:18:32 +0700

    Home page Herro section

--- assets/js/pkd-swiper-init.js
+++ assets/js/pkd-swiper-init.js
@@ -34,3 +34,15 @@ document.addEventListener("DOMContentLoaded", function () {
     });
   }
 });
+
+
+const cartButtons = document.querySelectorAll('.cart-button');
+
+cartButtons.forEach(button => {
+	button.addEventListener('click', cartClick);
+});
+
+function cartClick() {
+	let button = this;
+	button.classList.add('clicked');
+}
--- template-parts/component/hero-section.php
+++ template-parts/component/hero-section.php
@@ -1,19 +1,34 @@
-<div class="hero-section">
-    <div class="hero-section__header">
-        <div class="hero-section__categories">
-            <h1>Linh kiện máy tính</h1>
+<div class="hero-section__wrapper">
+    <div class="hero-section">
+        <div class="hero-section__content-left">
+            <div class="image-wrapper">
+                <img src="<?php echo get_template_directory_uri(); ?>/assets/images/hero-image1.png" alt="Hero Image" class="hero-section__image">
+            </div>
         </div>
-        <div class="hero-section__see-more">
-            <a href="#">Xem thêm >></a>
+        <div class="hero-section__content-right">
+            <div class="wrapper">
+
+                <div class="hero-section__content-right content-top">
+                    <div class="image-wrapper">
+                        <!-- <img src="<?php echo get_template_directory_uri(); ?>/assets/images/hero-image.png" alt="Hero Image" class="hero-section__image"> -->
+                        <h1>Top content</h1>
+                    </div>
+                </div>
+                <div class="hero-section__content-right content-left">
+                    <div class="image-wrapper">
+                        <img src="<?php echo get_template_directory_uri(); ?>/assets/images/hero-image.png" alt="Hero Image" class="hero-section__image">
+                          <h1>Left content</h1>
+                    </div>
+                </div>
+                <div class="hero-section__content-right content-right">
+                    <div class="image-wrapper">
+                        <img src="<?php echo get_template_directory_uri(); ?>/assets/images/hero-image.png" alt="Hero Image" class="hero-section__image">
+                          <h1>Right content</h1>
+                    </div>
+                </div>
+            </div>
         </div>
-    </div>
-    <div class="hero-section__line-break"></div>
-    <div class="hero-section__content">
-        <div class="content__img">
-            <img src="<?php echo get_template_directory_uri(); ?>/assets/images/laptop600x400.png" alt="Hero Image" class="hero-image" />
-        </div>
-        <div class="content__right">
-            <img src="<?php echo get_template_directory_uri(); ?>/assets/images/laptop600x400.png" alt="Hero Image" class="hero-image" />
-        </div>
+
     </div>
 </div>
+<?php
